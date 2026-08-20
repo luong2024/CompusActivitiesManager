6e89599e2972952fb23ba711c34a8754b8ae145b
ancuong444 <dtc245190004@ictu.edu.vn>
2026-08-20 09:12:42 +0700

    dky

--- CampusActivitiesManager/CampusActivitiesManager/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using CampusActivitiesManager.Data;
-global using CampusActivitiesManager.PageModels;
-global using CampusActivitiesManager.Pages;
-global using CampusActivitiesManager.Services;
-global using CampusActivitiesManager.Utilities;
-global using Fonts;
